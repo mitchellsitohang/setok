f2594802ce29eb72c4a7293e9b1944777fcf67b2
Mitchell Sitohang <mitchellsitohang@gmail.com>
2022-08-20 11:22:30 +0200

    reference to style file change from sass to scss

--- src/app/components/ling/ling.component.ts
+++ src/app/components/ling/ling.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ling',
   templateUrl: './ling.component.html',
-  styleUrls: ['./ling.component.sass']
+  styleUrls: ['./ling.component.scss']
 })
 export class LingComponent implements OnInit {
   public name: string = 'ling';
