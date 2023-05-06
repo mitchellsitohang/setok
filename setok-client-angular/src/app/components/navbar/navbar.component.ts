1c025255887f58ceef155ab00442cfeb0b65a87e
lingiej21 <lingiej21@gmail.com>
2023-05-06 10:23:39 +0200

    WIP

--- setok-client-angular/src/app/components/navbar/navbar.component.ts
+++ setok-client-angular/src/app/components/navbar/navbar.component.ts
@@ -14,17 +14,17 @@ export class NavbarComponent {
 
   menuAppear(): void {
     const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('.nav-menu');
+    const navList = document.querySelector('.nav-list');
 
     hamburger?.addEventListener('click', () => {
       hamburger?.classList.toggle('active');
-      navMenu?.classList.toggle('active');
+      navList?.classList.toggle('active');
     });
 
     document.querySelectorAll('.nav-link').forEach((n) =>
       n.addEventListener('click', () => {
         hamburger?.classList.remove('active');
-        navMenu?.classList.remove('active');
+        navList?.classList.remove('active');
       })
     );
   }
