783160ee8cc4349dfaad61465b0f4fdee53b4c6c
Mitchell Sitohang <mitchellsitohang@gmail.com>
2023-06-16 16:04:38 +0200

    fix menu keeps showing after navclick, now  menu is gone after click

--- setok-client-angular/src/app/components/navbar/navbar.component.ts
+++ setok-client-angular/src/app/components/navbar/navbar.component.ts
@@ -9,22 +9,11 @@ export class NavbarComponent {
   constructor() {}
 
   ngOnInit(): void {
-    this.menuAppear();
-  }
-
-  menuAppear(): void {
-    const hamburger = document.querySelector('.hamburger');
-    const navList = document.querySelector('.nav-list');
-
-    hamburger?.addEventListener('click', () => {
-      hamburger?.classList.toggle('active');
-      navList?.classList.toggle('active');
-    });
+    const check = document.querySelector('#check') as HTMLInputElement;
 
     document.querySelectorAll('.nav-link').forEach((n) =>
       n.addEventListener('click', () => {
-        hamburger?.classList.remove('active');
-        navList?.classList.remove('active');
+        check.checked = false;
       })
     );
   }
